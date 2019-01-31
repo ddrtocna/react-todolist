e582067bb79f73e04c95a4838cc614585b129a2b
imac04 <imac04@imac08.local>
2019-01-31 16:30:32 +0900

    Structure

---
+++ src/Component/Container/AppContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { App } from '../Components';
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <div>
+        <App />
+      </div>
+    );
+  }
+}
